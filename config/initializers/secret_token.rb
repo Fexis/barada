9352ebd8875ecfaae54901eba4aa1045115ac478
Fexis <nowthelove@gmail.com>
2013-05-07 20:43:09 +1100

    Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sss::Application.config.secret_token = '89df1728c4e0923cc44151a399c2516d822263da2240819df9be199d5ca23a2ae1ad8b54a818e6690d9f3b26b0b08f7ca2499234fdffb6f71502e8c10c2f82e7'
